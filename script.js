b6b6a2489bacc6d28b72149f662ba968227a58ff
Paula <caped.20201p2ads0226@aluno.ifpi.edu.br>
2023-02-27 19:25:35 -0300

    inicializando projeto

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    document.getElementById("frm1").submit();
+}
+
+
+function myFunction() {
+  alert("I am an alert box!");
+}
+
+
+function sum(...args) {
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum;
+  }
+  
+  let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
